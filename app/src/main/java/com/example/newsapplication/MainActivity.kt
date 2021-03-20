8f47a9c100181a5ad59852fa8c95bfdaa26ef8f6
Raghdaa al-amir <raghdaa2410@gmail.com>
2021-03-20 20:21:33 +0200

    creat splach screen and theme no action bar

--- app/src/main/java/com/example/newsapplication/MainActivity.kt
+++ app/src/main/java/com/example/newsapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.newsapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler(Looper.getMainLooper()).postDelayed({
+            gotoHomePage()
+        },2000)
+    }
+
+    private fun gotoHomePage() {
+     val intent = Intent(this,HomePage::class.java)
+        startActivity(intent)
+        finish()
     }
 }
